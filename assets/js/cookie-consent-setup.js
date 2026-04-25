08e0e175fc2c2b94afca18c86683ed514137e51d
shourovj <108189052+shourovj@users.noreply.github.com>
2026-04-25 15:29:27 +0000

    Deploying to gh-pages from @ jdickamazz/jdickamaaz.github.io@ee59ecde86e80cd2ec91a65edb0a8950eb409cbc 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://shourovj.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jdickamazz.github.io/jdickamaaz.github.io/#contact">contact us</a>.'
               }
             ]
           }
