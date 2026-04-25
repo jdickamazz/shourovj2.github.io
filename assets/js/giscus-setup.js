238cf22a4b03aeebf9f25e9d1cc5cf4562bec630
shourovj <108189052+shourovj@users.noreply.github.com>
2026-04-25 15:39:16 +0000

    Deploying to gh-pages from @ jdickamazz/shourovj2.github.io@b40da4fa8ff32bb573129bb16e8bce9ef3c13a1d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jdickamazz/jdickamaaz.github.io",
+    "data-repo": "jdickamazz/shourovj2.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
